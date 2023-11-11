2b2a7c5b78484eed9a6162e05a07d844de8b1779
Eduard Arnedo Hidalgo <eduardoarnedo90@gmail.com>
2023-11-11 19:34:07 +0100

    Add new use case register user (#6)
    Co-authored-by: Eduard Arnedo <earnedo@circutor.com>

--- business/usecase/user/contracts.go
+++ business/usecase/user/contracts.go
@@ -1,4 +1,3 @@
-// Package user to implement use case.
 package user
 
 import (
@@ -6,9 +5,9 @@ import (
 	"myc-devices-simulator/business/core/user"
 )
 
-//go:generate mockery --name CoreUser
-
 // CoreUser methods core user to store.
+//
+//go:generate mockery --name CoreUser
 type CoreUser interface {
 	Create(ctx context.Context, user user.User) (user.User, error)
 }
